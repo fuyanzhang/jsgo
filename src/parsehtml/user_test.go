6eb71def1448e27800cee6cfac5d03de34c73641
fuyanzhang <776372736@qq.com>
2018-01-21 19:09:52 +0800

    UT

--- src/parsehtml/user_test.go
+++ src/parsehtml/user_test.go
@@ -3,12 +3,15 @@ package parsehtml
 import (
 	"testing"
 	"fmt"
+	"strconv"
 )
 
 func TestGetUsersId(t *testing.T) {
 	uids := GetUsersId("https://www.jianshu.com/recommendations/users?page=1")
-	fmt.Println(len(uids))
-	fmt.Println(uids[0])
+	fmt.Println(uids)
+	fmt.Println(len(*uids))
+	fmt.Println(*uids)
+	fmt.Println((*uids)[0])
 }
 
 func TestGetUserInfo(t *testing.T) {
@@ -18,4 +21,18 @@ func TestGetUserInfo(t *testing.T) {
 	fmt.Println(userInfo.UserFollowing)
 	fmt.Println(userInfo.UserUrl)
 	fmt.Println(userInfo.NickName)
+}
+
+func TestGetUsersId2(t *testing.T) {
+	//获取2400个用户id
+	var allUIds []*[]string
+	path := "https://www.jianshu.com/recommendations/users?page="
+	for i:=1;i<=100 ;i++  {
+		url := path+strconv.Itoa(i)
+		uids := GetUsersId(url)
+		allUIds[i] = uids
+
+	}
+
+
 }
